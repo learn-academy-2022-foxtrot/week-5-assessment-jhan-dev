5312cc3b0ae33bab8ab179e142bbf12deccc1f29
Joseph <jhan.technology@gmail.com>
2022-10-07 19:48:18 -0700

    Updated pseudo-code & finished ruby-challenges for week-5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -24,18 +24,18 @@
 //  First create a Test function tha takes in a string, and returns a coded message string.
 describe('cipher', () => {
 
-    //  Create an it function that will explain what is the purpose of the 'cipher' function
+    //Create an it function that will explain what is the purpose of the 'cipher' function
     it('Create a function that takes in a string and returns a coded message.', () => {
 
-        //  Given 1st testCase is to equal the Expected output: "L4ck4d41s1c4l"
+        //Given 1st testCase is to equal the Expected output: "L4ck4d41s1c4l"
         const secretCodeWord1 = "Lackadaisical"
         expect(cipher(secretCodeWord1)).toEqual('L4ck4d41s1c4l')
         
-        //  Given 2nd testCase is to equal the Expected output: "G0bbl3dyg00k"
+        //Given 2nd testCase is to equal the Expected output: "G0bbl3dyg00k"
         const secretCodeWord2 = "Gobbledygook"
         expect(cipher(secretCodeWord2)).toEqual('G0bbl3dyg00k')
         
-        //  Given 3rd testCase is to equal the Expected output: "3cc3ntr1c"
+        //Given 3rd testCase is to equal the Expected output: "3cc3ntr1c"
         const secretCodeWord3 = "Eccentric"
         expect(cipher(secretCodeWord3)).toEqual('3cc3ntr1c')
         
@@ -49,35 +49,35 @@ describe('cipher', () => {
 
 //  To make the test pass, I will need to create the function I am testing for above and pass in a string parameter and convert "a" to 4, "e" to 3, "i" to 1, and "o" to 0 in the return string.
 const cipher = (codedMsg) => {
-    // Create empty string result varaible that will be our return
+    //Create empty string result varaible that will be our return
     let result = ''
 
-    //  Convert codedMsg string to array before iterating with .map() method
+    //Convert codedMsg string to array before iterating with .map() method
     codedMsg.split('').map((value => {
-        //  If the value = the char a --> 4
+        //If the value = the char a --> 4
         if (value.toLowerCase() === 'a') {
             result += '4'
         }
-        //  Else if the value = the char e --> 3
+        //Else if the value = the char e --> 3
         else if (value.toLowerCase() === 'e') {
             result += '3'
         }
-        //  Else if the value = the char i --> 1
+        //Else if the value = the char i --> 1
         else if (value.toLowerCase() === 'i') {
             result += '1'
         }
-        //  Else if the value = the char o --> 0
+        //Else if the value = the char o --> 0
         else if (value.toLowerCase() === 'o') {
             result += '0'
         }
-        //  Else concat current value to the result variable without cipher
+        //Else concat current value to the result variable without cipher
         else {
             result += value
         }
-        //  Convert the array back into a string
+        //Convert the array back into a string
     })).join('')
 
-    //  return result
+    //return result
     return result
 }
 
@@ -101,20 +101,20 @@ const cipher = (codedMsg) => {
 
 // a) Create a test with expects statement using the variable provided.
 
-//  First create a Test function tha takes in an array of words & a single letter, then returns an array of all words containing letter.
+//First create a Test function tha takes in an array of words & a single letter, then returns an array of all words containing letter.
 describe('checkForChar', () => {
 
-    //  Create an it function that will explain what is the purpose of the 'checkForChar' function
+    //Create an it function that will explain what is the purpose of the 'checkForChar' function
     it('Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter', () => {
 
-        //  Given testCase for 1st param
+        //Given testCase for 1st param
         const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
         
-        //  1st testCase for 2nd param: ["Mango", "Apricot", "Peach"]
+        //1st testCase for 2nd param: ["Mango", "Apricot", "Peach"]
         const letterA = "a"
         expect(checkForChar(fruitArray,letterA)).toEqual(["Mango", "Apricot", "Peach"])
         
-        //  2nd testCase for 2nd param: ["Cherry", "Blueberry", "Peach"]
+        //2nd testCase for 2nd param: ["Cherry", "Blueberry", "Peach"]
         const letterE = "e"
         expect(checkForChar(fruitArray,letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
 
@@ -127,23 +127,23 @@ describe('checkForChar', () => {
 
 // b) Create the function that makes the test pass.
 
-//  To make the test pass, I will need to create the function I am testing for above and pass in an array of strings and to filter only words in array with given 2nd param inside of string.
+//To make the test pass, I will need to create the function I am testing for above and pass in an array of strings and to filter only words in array with given 2nd param inside of string.
 const checkForChar = (arrOfWords, letter) => {
 
-    //  Convert all letter params to be toLowerCase()
+    //Convert all letter params to be toLowerCase()
     letter = letter.toLowerCase()
 
-    //  Create a result variable that declares arrOfWords to be iterated w/ the filter() Method
+    //Create a result variable that declares arrOfWords to be iterated w/ the filter() Method
     let result = arrOfWords.filter((word => {
 
-        //  If the word toLowerCase() includes() the letter param inside of it
+        //If the word toLowerCase() includes() the letter param inside of it
         if (word.toLowerCase().includes(letter)) {
 
-            //  Return the current word 
+            //Return the current word 
             return word
         }
     }))
-    //  Return result iteration after completion
+    //Return result iteration after completion
     return result
 
 }
@@ -168,25 +168,25 @@ const checkForChar = (arrOfWords, letter) => {
 
 // a) Create a test with expect statements using the variable provided.
 
-//  First create a Test function tha takes in an array of words & a single letter, then returns an array of all words containing letter.
+//First create a Test function tha takes in an array of words & a single letter, then returns an array of all words containing letter.
 describe('fullHouseCheck', () => {
 
-    //  Create an it function that will explain what is the purpose of the 'fullHouseCheck' function
+    //Create an it function that will explain what is the purpose of the 'fullHouseCheck' function
     it('Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind', () => {
 
-        //  Given 1st testCase is to equal the Expected output: true
+        //Given 1st testCase is to equal the Expected output: true
         const hand1 = [5, 5, 5, 3, 3]
         expect(fullHouseCheck(hand1)).toEqual(true)
         
-        //  Given 2nd testCase is to equal the Expected output: false
+        //Given 2nd testCase is to equal the Expected output: false
         const hand2 = [5, 5, 3, 3, 4]
         expect(fullHouseCheck(hand2)).toEqual(false)
         
-        //  Given 2nd testCase is to equal the Expected output: false
+        //Given 2nd testCase is to equal the Expected output: false
         const hand3 = [5, 5, 5, 5, 4]
         expect(fullHouseCheck(hand3)).toEqual(false)
         
-        //  Given 2nd testCase is to equal the Expected output: true
+        //Given 2nd testCase is to equal the Expected output: true
         const hand4 = [7, 2, 7, 2, 7]
         expect(fullHouseCheck(hand4)).toEqual(true)
     })   
@@ -200,15 +200,15 @@ describe('fullHouseCheck', () => {
 //  To make the test pass, I will need to create the function I am testing for above and pass in an array of 5 nums and to count 2 unique types of cards to have a pair and a 3 of a kind to return true or false if that condition is met.
 const fullHouseCheck = (hand) => {
 
-    //  Create an empty object to hold card and count
+    //Create an empty object to hold card and count
     let uniqueCards = {}
 
-    //  Iterate through the hand of cards
+    //Iterate through the hand of cards
     for (let i = 0; i < hand.length; i++) {
-        //  Create currentCard varaible --> represents current value iteration
+        //Create currentCard varaible --> represents current value iteration
         let currentCard = hand[i]
 
-        //  If the object is undefined --> aka empty {}
+        //If the object is undefined --> aka empty {}
         if (uniqueCards[currentCard] === undefined) {
             //  Insert currentCard into the object {} w/ a count of 1
             uniqueCards[currentCard] = 1 
@@ -220,15 +220,15 @@ const fullHouseCheck = (hand) => {
         }
     }
 
-    //  Iterate the object
+    //Iterate the object
     for (let count in uniqueCards) {
-        //  If the count of the card inside the object = 1
+        //If the count of the card inside the object = 1
         if (uniqueCards[count] === 1) {
-            //  Return false because only a pair(2) or (3) of a kind can only be present in a full house
+            //Return false because only a pair(2) and (3) of a kind can only be present in a full house
             return false
         }
     }
-    //  If the above condition is not met, JS will come to this line and return True
+    //If the above condition is not met, JS will come to this line and return True
     return true
 }
 
